7b51ff9925299d22fa75409494f502b1c4e06fb2
swanie21 <kswanie21@gmail.com>
2017-10-17 23:08:31 -0600

    make box frames

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Styles from './App.scss';
 import Header from '../../components/Header/Header';
 
 function App () {
   return (
     <div>
       <Header />
+      <div className={ Styles['box-container'] }>
+        <div className={ Styles['box-frame'] }></div>
+        <div className={ Styles['box-frame'] }></div>
+        <div className={ Styles['box-frame'] }></div>
+        <div className={ Styles['box-frame'] }></div>
+        <div className={ Styles['box-frame'] }></div>
+        <div className={ Styles['box-frame'] }></div>
+        <div className={ Styles['box-frame'] }></div>
+        <div className={ Styles['box-frame'] }></div>
+      </div>
     </div>
   );
 }
